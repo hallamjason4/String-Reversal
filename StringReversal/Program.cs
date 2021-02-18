20d3504421980fccabd3a5b8a3aaf4f196d5ea1a
hallamjason4 <70266362+hallamjason4@users.noreply.github.com>
2021-02-18 16:31:23 +0000

    Add files via upload
    Takes an input string and outputs it in its reversed state

---
+++ StringReversal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello World";
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            Console.WriteLine(charArray);
+            Console.ReadLine();
+        }
+    }
+}
